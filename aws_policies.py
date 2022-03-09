505f0bda1ad2d1f47c2fbd47b228c4bc0509ae84
Ankit Yadav <ankity@cloudera.com>
2022-03-09 12:31:45 -0500

    add comments and change variable names

--- aws_policies.py
+++ aws_policies.py
@@ -6,9 +6,15 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 iam = boto3.client('iam')
 
+#####Global Variables#####
+
+#TODO: Ensure bucket name is setup correctly in all policies and 
 LOGS_BUCEKT=config.get('S3', 'S3_BUCKET_ARN')
 LOGS_LOCATION_BASE=config.get('S3', 'S3_BUCKET_ARN')
-USERNAME = config.get('NAMES', 'USERNAME_PREFIX')
+USERNAME = config.get('NAMES', 'USERNAME_PREFIX') # Username will be appended to all policy names 
+
+
+#####AWS Policies#####
 
 aws_cdp_log_policy_document = {
 	"Version": "2012-10-17",
@@ -185,33 +191,29 @@ aws_cdp_backup_policy_document = {
 }
 
 
+# creating lists for policy names and policy documents
+
 policy_names= [USERNAME+"aws_cdp_log_policy", USERNAME+"aws_cdp_idbroker_assume_role_policy", USERNAME+"aws_cdp_ranger_audit_s3_policy", USERNAME+"aws_cdp_datalake_admin_s3_policy", USERNAME+"aws_cdp_bucket_access_policy", USERNAME+"aws_cdp_backup_policy"]
 
 policy_documents = [aws_cdp_log_policy_document, aws_cdp_idbroker_assume_role_policy_document, aws_cdp_ranger_audit_s3_policy_document, aws_cdp_datalake_admin_s3_policy_document, aws_cdp_bucket_access_policy_document, aws_cdp_backup_policy_document]
 
-zipped_files = zip(policy_names, policy_documents)
+#zip function pairs the first item of each list together
+
+zipped_files = zip(policy_names, policy_documents) 
+
+
+#####create_policy function creates IAM policies using policy names and policy documents#####
 
 def create_policy(zip_list):
-	for i,j in zipped_files:
+	for policy_names,policy_documents in zipped_files:
 		cdp_policy_create = iam.create_policy(
-			PolicyName=i,
-			PolicyDocument=json.dumps(j)
-	)
+			PolicyName=policy_names,
+			PolicyDocument=json.dumps(policy_documents)
+		)
+
+
 
 create_policy(zipped_files)
 
 
 #####Rough Code#####
-
-# def updateJsonFile():
-#     jsonFile = open("aws-cdp-log-policy1.json", "r") # Open the JSON file for reading
-#     data = json.load(jsonFile) # Read the JSON into the buffer    
-#     print(data["Statement"][1]["Resource"])
-#     data["Statement"][0]["Resource"] = LOGS_BUCEKT
-#     data["Statement"][1]["Resource"] = LOGS_LOCATION_BASE+"/*"
-#     ## Save our changes to JSON file
-#     jsonFile = open("aws-cdp-log-policy1.json", "w+")
-#     jsonFile.write(json.dumps(data))
-#     jsonFile.close()
-
-# updateJsonFile()
